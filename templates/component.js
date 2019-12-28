0fdc857077ea33ff27f3422143fdbaa0e22c40a3
Qian Chen <elgs1980@hotmail.com>
2019-12-27 22:35:37 -0800

    Updated component template.

--- templates/component.js
+++ templates/component.js
@@ -2,21 +2,33 @@ customElements.define('${component}',
   class extends HTMLElement {
     constructor() {
       super();
-      const template = document.querySelector('#${component}').content;
 
+      const template = document.querySelector('#${component}').content;
       // attach to shadow dom
       this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true));
-
       // attach to normal dom
       // this.appendChild(template.cloneNode(true));
     }
 
-    static get observedAttributes() {
-      return [];
-    }
+    // connectedCallback() {
+    //   console.log(this.isConnected);
+    //   console.log('Element added to page.');
+    // }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-      // console.log(name, oldValue, newValue);
-    }
+    // disconnectedCallback() {
+    //   console.log('Element removed from page.');
+    // }
+
+    // adoptedCallback() {
+    //   console.log('Element moved to new page.');
+    // }
+
+    // static get observedAttributes() {
+    //   return [];
+    // }
+
+    // attributeChangedCallback(name, oldValue, newValue) {
+    //   console.log(name, oldValue, newValue);
+    // }
   }
 );
