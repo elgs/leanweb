ebfad66b97fb45b2ea4f0626c4f5a062463e0738
Qian Chen <elgs1980@hotmail.com>
2020-01-02 02:48:42 -0800

    Added tokenizer for component2.

---
+++ tests/test-lw.js
@@ -0,0 +1,16 @@
+const lwlib = require('../lib/lw.js');
+
+describe('leanweb lib tests', () => {
+   it('should tokenize string', () => {
+      expect(lwlib.tokenize('1+1')).toEqual(['1', '+', '1']);
+      expect(lwlib.tokenize('1 + 1')).toEqual(['1', '+', '1']);
+      expect(lwlib.tokenize('2*x+y')).toEqual(['2', '*', 'x', '+', 'y']);
+      expect(lwlib.tokenize('-1+a*-b')).toEqual(['-1', '+', 'a', '*', '-b']);
+      expect(lwlib.tokenize('(-1+a)*-b')).toEqual(['(', '-1', '+', 'a', ')', '*', '-b']);
+      expect(lwlib.tokenize('a===b')).toEqual(['a', '===', 'b']);
+      expect(lwlib.tokenize('(a)===(b)')).toEqual(['(', 'a', ')', '===', '(', 'b', ')']);
+      expect(lwlib.tokenize('a==b')).toEqual(['a', '==', 'b']);
+      expect(lwlib.tokenize('a!=b')).toEqual(['a', '!=', 'b']);
+      expect(lwlib.tokenize('a!==b')).toEqual(['a', '!==', 'b']);
+   });
+});
