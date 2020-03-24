9854f942cb94cd1dd5cd20a7856875ddea4589bc
Qian Chen <elgs1980@hotmail.com>
2020-03-24 04:15:15 -0700

    Added port/host configration to serve.

--- commands/serve.js
+++ commands/serve.js
@@ -3,6 +3,17 @@ const utils = require('./utils.js');
 const liveServer = require("live-server");
 
 (async () => {
+
+   let port = 2020;
+   let host = '127.0.0.1';
+
+   if (process.argv.length > 2) {
+      port = process.argv[2] * 1;
+   }
+   if (process.argv.length > 3) {
+      host = process.argv[3];
+   }
+
    const build = (eventType, filename) => {
       // console.log(eventType + ': ', filename);
       utils.exec(`npx leanweb build`);
@@ -14,12 +25,10 @@ const liveServer = require("live-server");
    });
 
    const params = {
-      port: 2020,
-      host: "127.0.0.1",
-      root: process.cwd() + "/build/",
+      port,
+      host,
+      root: process.cwd() + "/",
       open: true,
-      ignore: 'node_modules/', // comma-separated string for paths to ignore
-      file: "index.html",
       wait: 1000,
       logLevel: 0, // 0 = errors only, 1 = some, 2 = lots
    };
