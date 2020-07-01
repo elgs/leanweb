950b60e4b85241735dcdf11b0253f83d8215d3f3
Qian Chen <elgs1980@hotmail.com>
2020-07-01 06:47:54 -0700

    Bind update and applyStyles in LWElement.

--- templates/lib/lw-element.js
+++ templates/lib/lw-element.js
@@ -134,7 +134,7 @@ export default class LWElement extends HTMLElement {
    }
 
    async _bindMethods() {
-      const methodNames = [];
+      const methodNames = ['update', 'applyStyles'];
       const proto = Object.getPrototypeOf(this);
       methodNames.push(...Object.getOwnPropertyNames(proto).filter(name => hasMethod(proto, name)));
       methodNames.push(...Object.getOwnPropertyNames(this).filter(name => hasMethod(this, name)));
